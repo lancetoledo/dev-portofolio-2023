8ceb21271692f6adb7e6ea5a719f04711756b3a6
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2024-10-15 01:19:25 -0400

    MODIFY: SIngle component

--- src/SingleFeatures/SIngleCountdown.jsx
+++ src/SingleFeatures/SIngleCountdown.jsx
@@ -1,26 +1,26 @@
-//src/SingleFeatures/SingleCountdown.jsx
-
 import React from 'react';
 import CountUp from 'react-countup';
 import VisibilitySensor from "react-visibility-sensor";
 
-export default function SIngleCountdown(props) {
+export default function SingleCountdown(props) {
     const { title, start, end, duration, extraClass, formattedValue, children } = props;
     
     return (
         <div className={extraClass ? `col-md-3 col-sm-6 ${extraClass}` : "col-md-3 col-sm-6"}>
             <div className="counter-item text-center">
-                {children} {/* This will render the icon */}
+                {children && <div className="icon-wrapper">{children}</div>}
                 <VisibilitySensor>
-                    {({ isVisible }) =>
+                    {({ isVisible }) => (
                         <h4 className="pc"> 
-                            {isVisible ? (
-                                <span className="count">
-                                    {formattedValue || <CountUp start={start} end={end} duration={duration} formattingFn={(value) => value.toLocaleString()} />}
-                                </span>
-                            ) : "0"}
+                            <span className="count">
+                                {isVisible
+                                    ? (formattedValue !== undefined && formattedValue !== null
+                                        ? formattedValue
+                                        : <CountUp start={start} end={end} duration={duration} formattingFn={(value) => value.toLocaleString()} />)
+                                    : start}
+                            </span>
                         </h4>
-                    }
+                    )}
                 </VisibilitySensor>
                 <p>{title}</p>
             </div>
